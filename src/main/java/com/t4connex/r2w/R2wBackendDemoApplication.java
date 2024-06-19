306e7744157ecdb96d2c390c58bef79a4691bfb4
HemaKumar <hema.kumar@t4connex.com>
2024-06-19 12:49:23 +0100

    initial commit

---
+++ src/main/java/com/t4connex/r2w/R2wBackendDemoApplication.java
@@ -0,0 +1,13 @@
+package com.t4connex.r2w;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2wBackendDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2wBackendDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t4connex/r2w/R2wBackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t4connex.r2w;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2wBackendDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
